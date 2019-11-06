b6dfbdb98a21d168829391800a385ed809650db7
Yussef Seddik <yussefseddik@gmail.com>
2019-11-06 18:29:50 +0200

    Update ApiInterface.java

--- app/src/main/java/com/alryada/etqan/Retofit/ApiInterface.java
+++ app/src/main/java/com/alryada/etqan/Retofit/ApiInterface.java
@@ -8,10 +8,7 @@ import retrofit2.http.Field;
 import retrofit2.http.FormUrlEncoded;
 import retrofit2.http.POST;
 
-/**
- * Created by Abd El-Sattar on
- * 8/4/2017.
- */
+
 
 public interface ApiInterface {
 
